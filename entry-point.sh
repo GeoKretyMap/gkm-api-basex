2e9d4c02f914f6ddcfca822504918673379c2068
Mathieu Alorent <github@kumy.net>
2016-06-05 22:09:13 +0000

    Create export directory

--- entry-point.sh
+++ entry-point.sh
@@ -3,8 +3,9 @@ set -eo pipefail
 
 # allow the container to be started with `--user`
 if [ "$(id -u)" = '0' ]; then
-	chown -R basex. /srv/BaseXData /srv/BaseXRepo
-	gosu basex "$@"
+   [ ! -d /srv/BaseXData/export/gkdetails ] && mkdir -p /srv/BaseXData/export/gkdetails
+   chown basex. /srv/BaseXData /srv/BaseXRepo /srv/BaseXData/export /srv/BaseXData/export/gkdetails
+   gosu basex "$@"
 fi
 
 exec "$@"
